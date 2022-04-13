45016ba8ad29642acf4efe6d5b4f23e9a5c01a0f
Chronos <chronos.ohm@protonmail.com>
2022-04-13 14:01:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ process.on("unhandledRejection", (reason, promise) => {
 client.login(process.env.TOKEN);
 
 // Set up endpoint on "/" to get green status on EB
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 server.listen(port);
 console.log('Server running at http://127.0.0.1:' + port + '/');
